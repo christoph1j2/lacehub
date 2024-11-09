8bce499e4e0e33979fdfc508da30b4c039226f3b
Ernst Christoph Leschka <ernstleska@gmail.com>
2024-11-09 12:09:03 +0100

    users controller unit test

--- app-backend/src/users/users.controller.ts
+++ app-backend/src/users/users.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Post, UseGuards, Request, Patch, ParseIntPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard/jwt-auth.guard';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard/jwt-auth.guard';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
