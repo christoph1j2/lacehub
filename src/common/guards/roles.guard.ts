632c966b35928b08ea2bd8eed49fb380e5d10136
Ernst Christoph Leschka <ernstleska@gmail.com>
2024-11-16 21:53:21 +0100

    added jsdoc to roles guard

--- src/common/guards/roles.guard.ts
+++ src/common/guards/roles.guard.ts
@@ -6,6 +6,13 @@ import { ROLES_KEY } from '../decorators/roles.decorator';
 export class RolesGuard implements CanActivate {
     constructor(private reflector: Reflector) {}
 
+    /**
+     * This method is part of the CanActivate interface and is used by the Guard to determine
+     * if the request should be allowed to proceed. It checks if the user has any of the roles
+     * specified in the @Roles decorator.
+     * @param context The ExecutionContext of the request.
+     * @returns `true` if the request should proceed, `false` otherwise.
+     */
     canActivate(context: ExecutionContext): boolean {
         const requiredRoles = this.reflector.getAllAndOverride<string[]>(
             ROLES_KEY,
