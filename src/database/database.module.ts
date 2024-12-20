ce100a8404827066b2658d38fa3e783459c998b0
christoph1j2 <ernstleska@gmail.com>
2024-12-20 12:41:14 +0100

    fixed

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -4,7 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import * as os from 'os';
 
 const localIP = getLocalIP();
-const isServerIP = localIP === '93.99.25.8';
+const isServerIP = localIP === '172.20.0.7';
 
 console.log('localIP', localIP);
 console.log('isServerIP', isServerIP);
