260ad8e75f3269ed34c54d6b731d9d057f5fc4db
Ernst Christoph Leschka <ernstleska@gmail.com>
2024-12-25 13:41:32 +0100

    changes

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -4,11 +4,10 @@ import { Notification } from '../entities/notification.entity';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
 import { User } from '../entities/user.entity';
-import { MailModule } from '../mail/mail.module';
 import { MailService } from 'src/mail/mail.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Notification, User]), MailModule],
+    imports: [TypeOrmModule.forFeature([Notification, User])],
     controllers: [NotificationsController],
     providers: [NotificationsService, MailService],
 })
