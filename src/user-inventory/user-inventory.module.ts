4d076260f866c8d277164af0bcad850ca3ed5dcc
christoph1j2 <ernstleska@gmail.com>
2025-01-21 22:01:27 +0100

    Fixed startup error

--- src/user-inventory/user-inventory.module.ts
+++ src/user-inventory/user-inventory.module.ts
@@ -5,10 +5,16 @@ import { UserInventoryController } from './user-inventory.controller';
 import { UserInventoryService } from './user-inventory.service';
 import { Product } from '../entities/product.entity';
 import { User } from '../entities/user.entity';
+import { WtbService } from 'src/wtb/wtb.service';
+import { WtsService } from 'src/wts/wts.service';
+import { Wtb } from 'src/entities/wtb.entity';
+import { Wts } from 'src/entities/wts.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([UserInventory, User, Product])],
+    imports: [
+        TypeOrmModule.forFeature([UserInventory, User, Product, Wts, Wtb]),
+    ],
     controllers: [UserInventoryController],
-    providers: [UserInventoryService],
+    providers: [UserInventoryService, WtbService, WtsService],
 })
 export class UserInventoryModule {}
