c04bf7e2d72945ed306d6c0a50a6dcdbf5c28aab
christoph1j2 <ernstleska@gmail.com>
2025-01-24 10:04:14 +0100

    rate limiter

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.use(cookieParser());
 
+    // Swagger setup
     if (process.env.NODE_ENV !== 'production') {
         const config = new DocumentBuilder()
             .setTitle('LaceHub API')
@@ -24,6 +26,14 @@ async function bootstrap() {
         });
     }
 
+    // Rate limiter
+    const limiter = rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 500, // limit each IP to 500 requests per windowMs
+    });
+    app.use(limiter);
+
+    // CORS
     app.enableCors({
         origin: ['http://localhost:5173', 'https://www.lacehub.cz'],
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
